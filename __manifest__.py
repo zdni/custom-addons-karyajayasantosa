1d8aa20ebe2606667d01e3638fc2db1a49e6ff7c
zdni <alzidni.kasim@gmail.com>
2022-11-10 20:44:23 +0800

    push

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Product Category Consigment',
+    'author': 'TechnoIndo',
+    'category': 'Report',
+    'version': '10.0',
+    'summary': 'Product Category Consigment',
+    'depends': ['product'],
+    'data': [
+        'views/product_category.xml',
+    ],
+    'installable': True,
+    'application': False,
+}

4e8d613539c65bd4d993a0cb0f606669290b34e8
zdni <alzidni.kasim@gmail.com>
2022-11-10 20:03:34 +0800

    push

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Account Invoice PO Fixed Discount',
+    'author': 'technoindo.com',
+    'category': 'hidden',
+    'version': '10.0',
+    'summary': 'Summary the addon.',
+    'description': '''Description the addon'''
+                   ,
+    'depends': ['account'],
+    'data': [
+        'views/account_invoice_view.xml'
+    ],
+    'images': [''],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}

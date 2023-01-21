d4045c97b8ae44f5983b8707c899728c80552750
zdni <alzidni.kasim@gmail.com>
2023-01-21 15:03:22 +0800

    bug multi user

--- models/res_users.py
+++ models/res_users.py
@@ -8,30 +8,29 @@ class User(models.Model):
 
     @api.multi
     def write(self, vals):
-        old_pin = '|' + str( self.id ) + ':|'
-        if self.pos_security_pin:
-            old_pin = '|' + str( self.id ) + ':' + self.pos_security_pin + '|'
-        
-        new_pin = '|' + str( self.id ) + ':|'
-        if vals.get('pos_security_pin'):
-            new_pin = '|' + str( self.id ) + ':' + vals.get('pos_security_pin') + '|'
-        
-        where_like_pin = '%' + old_pin + '%'
-        query = """
-            SELECT id, string_pin
-            FROM pos_config
-            WHERE string_pin LIKE %s
-        """
-        params = [ where_like_pin ]
-        self.env.cr.execute( query, params )
-        pos_configs = self.env.cr.dictfetchall()
-
-        for pos_config in pos_configs:
-            pos_config = self.env['pos.config'].search([('id', '=', pos_config['id'])])
-            replace_pin = pos_config['string_pin'].replace( old_pin, new_pin )
-            pos_config.write({
-                'string_pin': replace_pin,
-            })
+        for record in self:
+            super(User, record).write(vals)
+            old_pin = '|' + str( record.id ) + ':|'
+            if record.pos_security_pin:
+                old_pin = '|' + str( record.id ) + ':' + record.pos_security_pin + '|'
             
-        result = super(User, self).write(vals)
-        return result
+            new_pin = '|' + str( record.id ) + ':|'
+            if vals.get('pos_security_pin'):
+                new_pin = '|' + str( record.id ) + ':' + vals.get('pos_security_pin') + '|'
+            
+            where_like_pin = '%' + old_pin + '%'
+            query = """
+                SELECT id, string_pin
+                FROM pos_config
+                WHERE string_pin LIKE %s
+            """
+            params = [ where_like_pin ]
+            self.env.cr.execute( query, params )
+            pos_configs = self.env.cr.dictfetchall()
+
+            for pos_config in pos_configs:
+                pos_config = self.env['pos.config'].search([('id', '=', pos_config['id'])])
+                replace_pin = pos_config['string_pin'].replace( old_pin, new_pin )
+                pos_config.write({
+                    'string_pin': replace_pin,
+                })

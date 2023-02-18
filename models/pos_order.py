fa9b624282acc0208690e997520ca369f710675c
zdni <alzidni.kasim@gmail.com>
2023-02-18 17:56:55 +0800

    update

--- models/pos_order.py
+++ models/pos_order.py
@@ -12,15 +12,8 @@ class PosOrder(models.Model):
 
     @api.model
     def _order_fields(self, ui_order):
-        process_line = partial(self.env['pos.order.line']._order_line_fields)
-        return {
-            'name':         ui_order['name'],
-            'user_id':      ui_order['user_id'] or False,
-            'session_id':   ui_order['pos_session_id'],
-            'lines':        [process_line(l) for l in ui_order['lines']] if ui_order['lines'] else False,
-            'pos_reference': ui_order['name'],
-            'partner_id':   ui_order['partner_id'] or False,
-            'date_order':   ui_order['creation_date'],
-            'fiscal_position_id': ui_order['fiscal_position_id'],
+        res = super(PosOrder, self)._order_fields(ui_order)
+        res.update({
             'percent_discount':   ui_order['percent_discount'] or False,
-        } 
+        })
+        return res
